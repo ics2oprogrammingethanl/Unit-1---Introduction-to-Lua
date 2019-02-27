62e804da1578a1fc25a1666c04358c48f57f489e
Ethan LeGault <47784129+s256202@users.noreply.github.com>
2019-02-27 09:44:13 -0500

    DisplayShapes
    aa

--- main.lua
+++ main.lua
@@ -18,6 +18,23 @@ myPentagon.anchorX = 256
 myPentagon.anchorY = 0
 myPentagon.x = 256
 myPentagon.y = 20
+
+local paintPentagon = { 1, 0, 0, }
+myPentagon.fill = paintPentagon
+
+local pentagonBorder = { 0, 0, 1, }
+
+myPentagon.stroke = pentagonBorder
+myPentagon.strokeWidth = 25
+
+local pentagonText
+local pentagonTextSize = 40
+pentagonText = display.newText("Pentagon", 0, 0, Arial, pentagonTextSize)
+
+pentagonText.anchorX = 0
+pentagonText.anchorY = 0
+pentagonText.x = 100
+pentagonText.y = 300
 ---------------------------------------------------------------------------------------
 local halfW = display.contentWidth * 0.5
 local halfH = display.contentHeight * 0.5
@@ -31,6 +48,24 @@ myHexagon.anchorX = 512
 myHexagon.anchorY = 0
 myHexagon.x = 512
 myHexagon.y = 20
+
+local hexagonPaint = {
+	type = "composite",
+	paint1 = { type="image", filename="waterCover.jpg" },
+	paint2 = { type="image", filename="Large_bonfire.jpg" }
+}
+
+myHexagon.fill = hexagonPaint
+myHexagon.fill.effect = "composite.average"
+
+local hexagonText
+local hexagonTextSize = 40
+hexagonText = display.newText("Hexagon", 0, 0, Arial, hexagonTextSize)
+
+hexagonText.anchorX = 0
+hexagonText.anchorY = 0
+hexagonText.x = 350
+hexagonText.y = 300
 ---------------------------------------------------------------------------------------
 local halfW = display.contentWidth * 0.5
 local halfH = display.contentHeight * 0.5
@@ -44,7 +79,17 @@ myOctogon.anchorX = 768
 myOctogon.anchorY = 0
 myOctogon.x = 768
 myOctogon.y = 20
+
+local octogonPaint = {
+	type = "gradient",
+	color1 = { 1,0.5,0 },
+	color2 = { 1,0.1,0, 0.3 },
+	direction = "down"
+}
+myOctogon.fill = octogonPaint
 ---------------------------------------------------------------------------------------
+
+-- everything up to the next comment is about the size/dimentions/vertices for the triangle
 local halfW = display.contentWidth * 0.5
 local halfH = display.contentHeight * 0.5
 
@@ -57,3 +102,33 @@ myTriangle.anchorX = 1024
 myTriangle.anchorY = 0
 myTriangle.x = 1024
 myTriangle.y = 20
+
+-- create my area/text variables
+local baseOfTriangle = 20
+local heightOfTriangle = 30
+local areaOfTriangle
+local textSize = 50
+
+-- calculate the area of the triangle
+areaOfTriangle = baseOfTriangle * heightOfTriangle /2
+
+-- write the area on the screen.
+areaText = display.newText("The area of this triangle with a base of \n" ..
+	baseOfTriangle .. " and a height of " .. heightOfTriangle .. " is " ..
+	areaOfTriangle .. " pixels².", 0, 0, Arial, textSize)
+
+-- anchor the text and set its (x,y) position
+areaText.anchorX = 0
+areaText.anchorY = 0
+areaText.x = 20
+areaText.y = display.contentHeight/2
+
+-- create the image variable that is going inside the triangle
+local trianglePaint = {
+	type = "image",
+	filename = "broke.png",
+}
+
+-- fill the triangle
+myTriangle.fill = trianglePaint
+--------------------------------------------------------------------------------------
